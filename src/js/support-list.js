087eedfa2e3f948f630b4f380665fd4e0319826c
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-05-10 00:23:51 +0200

    Update support-list.js

--- src/js/support-list.js
+++ src/js/support-list.js
@@ -1,47 +1,56 @@
-[
+export const supportArr = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: null,
+    img: '../images/support-ukraine/save-the-children.png',
+    img2x: '../images/support-ukraine/save-the-children@2x.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: null,
+    img: '../images/support-ukraine/project-hope.png',
+    img2x: '../images/support-ukraine/project-hope@2x.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: null,
+    img: '../images/support-ukraine/united24.png',
+    img2x: '../images/support-ukraine/united24@2x.png',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: null,
+    img: '../images/support-ukraine/international-corps.png',
+    img2x: '../images/support-ukraine/international-corps@2x.png',
   },
   {
-    title: 'Medicins Sans Frontieres',
+    title: 'Medecins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: null,
+    img: '../images/support-ukraine/medecins-sans.png',
+    img2x: '../images/support-ukraine/medecins-sans@2x.png',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: null,
+    img: '../images/support-ukraine/razom.png',
+    img2x: '../images/support-ukraine/razom@2x.png',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: null,
+    img: '../images/support-ukraine/against-hunger.png',
+    img2x: '../images/support-ukraine/against-hunger@2x.png',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: null,
+    img: '../images/support-ukraine/world-vision.png',
+    img2x: '../images/support-ukraine/world-vision@2x.png',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: null,
+    img: '../images/support-ukraine/prytula.png',
+    img2x: '../images/support-ukraine/prytula@2x.png',
   },
 ];
