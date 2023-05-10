0acbd9b004df9288ce40667d7f96226c956458fd
talask <35809884+talask@users.noreply.github.com>
2023-05-10 15:48:17 +0300

    fixed api

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import axios from 'axios';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { getAnalytics } from 'firebase/analytics';
+
+import getBookAPI from './js/getBookAPI';
+async function showGallery () {
+
+ // const data = await getBookAPI('bookId', '643282b1e85766588626a085');
+ //const data = await getBookAPI('all');
+ const data = await getBookAPI('category', 'Picture Books');
+  return data;
+}
+console.log(showGallery());
--- src/js/getBookAPI.js
+++ src/js/getBookAPI.js
@@ -7,27 +7,25 @@ const URL = {
     bookId: 'https://books-backend.p.goit.global/books/',
 };
 
-export default async function getBookAPI(type, value) {
-    const param = value ? value : '';
-    console.log(type, value, `${URL[type]}${param}`);
-    try {
-      const response = await axios.get(`${URL[type]}${param}`);
+export default async function getBookAPI(type, value='') {
+    
+    console.log(type, value, `${URL[type]}${value}`);
+    
+      const response = await axios.get(`${URL[type]}${value}`);
       
       return response.data;
 
-    } catch (error) {
-        
-      throw Error (error.status);
-
-    }
+   
   }
 
   //Example include this function
 
-//   import getBookAPI from './js/getBookAPI';
-//   async function showGallery () {
+//    import getBookAPI from './js/getBookAPI';
+//    async function showGallery () {
 
-//     const data = await getBookAPI('bookId', '643282b1e85766588626a085');
-//     return data;
-// }
-// console.log(showGallery());
+//      const data = await getBookAPI('bookId', '643282b1e85766588626a085');
+//      const data = await getBookAPI('all');
+//      const data = await getBookAPI('category', 'Picture Books');
+//      return data;
+//  }
+//  console.log(showGallery());
