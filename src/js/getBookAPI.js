905681a79e72f897393ad8eff34a3dece717f8da
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-05-10 15:11:46 +0200

    Update getBookAPI.js

--- src/js/getBookAPI.js
+++ src/js/getBookAPI.js
@@ -1,24 +1,18 @@
 import axios from 'axios';
 
 const URL = {
-    all: 'https://books-backend.p.goit.global/books/category-list',
-    top: 'https://books-backend.p.goit.global/books/top-books',
-    category: 'https://books-backend.p.goit.global/books/category?category=',
-    bookId: 'https://books-backend.p.goit.global/books/',
+  all: 'https://books-backend.p.goit.global/books/category-list',
+  top: 'https://books-backend.p.goit.global/books/top-books',
+  category: 'https://books-backend.p.goit.global/books/category?category=',
+  bookId: 'https://books-backend.p.goit.global/books/',
 };
 
-export default async function getBookAPI(type, value='') {
-    
-    console.log(type, value, `${URL[type]}${value}`);
-    
-      const response = await axios.get(`${URL[type]}${value}`);
-      
-      return response.data;
+export async function getBookAPI(type, value = '') {
+  const response = await axios.get(`${URL[type]}${value}`);
+  return response.data;
+}
 
-   
-  }
-
-  //Example include this function
+//Example include this function
 
 //    import getBookAPI from './js/getBookAPI';
 //    async function showGallery () {
