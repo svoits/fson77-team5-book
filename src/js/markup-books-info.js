67dc2937cd88089ae30df0d551308331b9057038
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-12 00:48:59 +0300

    Update markup-books-info.js

--- src/js/markup-books-info.js
+++ src/js/markup-books-info.js
@@ -1,5 +1,7 @@
 import getBookAPI from "./getBookAPI";
 
-async function createBookInfoCard() {
-    const data = await getBookAPI('bookId');
-}
+// async function createBookInfoCard({ }) {
+//     const data = await getBookAPI('bookId');
+// }
+
+console.log(getBookAPI('bookId'));
