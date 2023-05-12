066ab455f02e1199e326201f53c2e443a94a0c58
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-12 17:26:02 +0300

    Create render-book-info.js

---
+++ src/js/render-book-info.js
@@ -0,0 +1,14 @@
+import { markupCardBookInfo } from "./markup-books-info";
+
+
+// let addRemoveBookButton = document.querySelector('#addRemoveBookButton');
+
+// addRemoveBookButton.addEventListener('click', (e) => {
+//   if (addRemoveBookButton.innerHTML === 'ADD TO SHOPPING LIST') {
+//     addRemoveBookButton.innerHTML = 'REMOVE FROM SHOPPING LIST';
+//     document.querySelector('#addRemoveBookButton').innerHTML = 'Congratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.';
+//   } else {
+//     addRemoveBookButton.innerHTML = 'ADD TO SHOPPING LIST';
+//     document.querySelector('#addRemoveBookButton').innerHTML = '';
+//   }
+// });
