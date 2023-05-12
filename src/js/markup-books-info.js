5118893166ea1446f1738adc7833951f8d032c95
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-13 00:02:51 +0300

    Update markup-books-info.js

--- src/js/markup-books-info.js
+++ src/js/markup-books-info.js
@@ -1,5 +1,9 @@
-export async function markupCardBookInfo(id) {
-    const markupCard = id.map(({ book_image, list_name, author, description, title, buy_links: { name, url } }) => {
+export async function markupCardBookInfo(_id) {
+    return _id.map(({ book_image, list_name, author, description, title, buy_links }) => {
+        const arr = buy_links.forEach((element) => {
+            console.log(element.name);
+            console.log(element.url);
+        });
         return `
         <div class="book_info_card">
             <h2 class="book_info_name">${list_name}</h2>
@@ -10,9 +14,7 @@ export async function markupCardBookInfo(id) {
             <button class="book_add__to_list" type="button" id="addRemoveBookButton">ADD TO SHOPPING LIST</button>
         </div>`
     }).join('');
-
-    return markupCard;
 }
 
-console.log(markupCardBookInfo());
-{/* <button class="book_remove_from_list" type="button">REMOVE FROM SHOPPING LIST</button> */}
+
+// buy_links: { name, url }
