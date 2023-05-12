15f3be7a50828786d5653935cac1a02895eff99a
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-13 00:02:55 +0300

    Update render-book-info.js

--- src/js/render-book-info.js
+++ src/js/render-book-info.js
@@ -1,14 +1,27 @@
 import { markupCardBookInfo } from "./markup-books-info";
 
+function onBookInfoClick(evt) {
+  // if(evt.target !== ) {
+  //   return;
+  // }
 
-// let addRemoveBookButton = document.querySelector('#addRemoveBookButton');
+  
+}
 
-// addRemoveBookButton.addEventListener('click', (e) => {
-//   if (addRemoveBookButton.innerHTML === 'ADD TO SHOPPING LIST') {
-//     addRemoveBookButton.innerHTML = 'REMOVE FROM SHOPPING LIST';
-//     document.querySelector('#addRemoveBookButton').innerHTML = 'Congratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.';
-//   } else {
-//     addRemoveBookButton.innerHTML = 'ADD TO SHOPPING LIST';
-//     document.querySelector('#addRemoveBookButton').innerHTML = '';
-//   }
-// });
+
+let addRemoveBookButton = document.querySelector('#addRemoveBookButton');
+
+addRemoveBookButton.addEventListener('click', (e) => {
+  if (addRemoveBookButton.innerHTML === 'ADD TO SHOPPING LIST') {
+    addRemoveBookButton.innerHTML = 'REMOVE FROM SHOPPING LIST';
+    document.querySelector('#addRemoveBookButton').innerHTML = 'Congratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.';
+  } else {
+    addRemoveBookButton.innerHTML = 'ADD TO SHOPPING LIST';
+    document.querySelector('#addRemoveBookButton').innerHTML = '';
+  }
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
