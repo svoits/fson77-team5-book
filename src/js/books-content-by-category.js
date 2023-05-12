ac4ec433685071681a6e98d022e018a06930416f
YuriiRodenko <jury.rodenko@gmail.com>
2023-05-12 12:24:22 +0300

    add markup for books by category
    главное работает!

--- src/js/books-content-by-category.js
+++ src/js/books-content-by-category.js
@@ -1,19 +1,24 @@
+import Notiflix from 'notiflix';
 import getBookAPI from "./getBookAPI";
+import { createListCardsTop } from "./createListCardsTop";
+
+const contentWrapper = document.querySelector('.js-content-wrapper');
 
 export default async function renderBooksByCategory(currentCategory) {
   try {
     const data = await getBookAPI('category', currentCategory);
-    console.log(data);
-  
+
+    if (!data.length) {
+      contentWrapper.innerHTML = '';
+
+      Notiflix.Notify.failure('Oops... Empty result');
+      return;
+    }
+
+    contentWrapper.innerHTML = createListCardsTop(data);
+    console.log(contentWrapper);
+
   } catch (err) {
     console.log(err);
   }
 }
-
-
-
-
-
-
-// 3. Данные перебрать map+join
-// 4. Разметка данных
