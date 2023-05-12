b302490bf3160f0fd198d2230210fa9a5732a647
Illia Shulha <ilyxa2836@gmail.com>
2023-05-13 00:36:44 +0200

    adds main styles for header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getAnalytics } from 'firebase/analytics';
+import './js/header';
