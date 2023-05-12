ecfa7b15144c38bfd95a46e723c7fe4e2134dfe8
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-12 17:26:10 +0300

    Update markup-books-info.js

--- src/js/markup-books-info.js
+++ src/js/markup-books-info.js
@@ -1,17 +1,18 @@
-import getBookAPI from './js/getBookAPI';
-
-async function markupCardBookInfo() {
-    const data = await getBookAPI('bookId');
-
-    const markupCard = data.map(({ book_image, list_name, author, description }) => {
+export async function markupCardBookInfo(id) {
+    const markupCard = id.map(({ book_image, list_name, author, description, title, buy_links: { name, url } }) => {
         return `
         <div class="book_info_card">
             <h2 class="book_info_name">${list_name}</h2>
-            <img src="${book_image}" alt="${list_name}">
+            <img src="${book_image}" alt="${title}">
             <p class="book_info_author">${author}</p>
             <p class="book_info_description">${description}</p>
-            // <a href="">${}</a><a href="">${}</a><a href="">${}</a>
-            <button class="book_add_shopping-list" type="button">ADD TO SHOPPING LIST</button>
+            <a href="${url}">${name}</a><a href="${url}">${name}</a><a href="${url}">${name}</a>
+            <button class="book_add__to_list" type="button" id="addRemoveBookButton">ADD TO SHOPPING LIST</button>
         </div>`
-    })
+    }).join('');
+
+    return markupCard;
 }
+
+console.log(markupCardBookInfo());
+{/* <button class="book_remove_from_list" type="button">REMOVE FROM SHOPPING LIST</button> */}
