5dace61576c1ff8aeaebdb96077da664cc1bbed0
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-13 01:16:13 +0300

    Update markup-books-info.js

--- src/js/markup-books-info.js
+++ src/js/markup-books-info.js
@@ -1,20 +1,12 @@
-export async function markupCardBookInfo(_id) {
-    return _id.map(({ book_image, list_name, author, description, title, buy_links }) => {
-        const arr = buy_links.forEach((element) => {
-            console.log(element.name);
-            console.log(element.url);
-        });
-        return `
-        <div class="book_info_card">
-            <h2 class="book_info_name">${list_name}</h2>
-            <img src="${book_image}" alt="${title}">
-            <p class="book_info_author">${author}</p>
-            <p class="book_info_description">${description}</p>
-            <a href="${url}">${name}</a><a href="${url}">${name}</a><a href="${url}">${name}</a>
-            <button class="book_add__to_list" type="button" id="addRemoveBookButton">ADD TO SHOPPING LIST</button>
-        </div>`
-    }).join('');
+export function markupCardBookInfo(data) {
+    const { book_image, list_name, author, description, title, buy_links } = data;
+    return `
+    <div class="book_info_card">
+        <h2 class="book_info_name">${list_name}</h2>
+        <img src="${book_image}" alt="${title}">
+        <p class="book_info_author">${author}</p>
+        <p class="book_info_description">${description}</p>
+        <a href="${buy_links[0].url}">${buy_links[0].name}</a><a href="${buy_links[1].url}">${buy_links[1].name}</a><a href="${buy_links[4].url}">${buy_links[4].name}</a>
+        <button class="book_add__to_list" type="button" id="addRemoveBookButton">ADD TO SHOPPING LIST</button>
+    </div>`
 }
-
-
-// buy_links: { name, url }
