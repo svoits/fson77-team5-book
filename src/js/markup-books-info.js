d54caf996ae03b44fbdd1e33809db7a4669adfdc
Alla Kaplia <kaplia.alla18@gmail.com>
2023-05-13 23:25:03 +0300

    Update markup-books-info.js

--- src/js/markup-books-info.js
+++ src/js/markup-books-info.js
@@ -6,12 +6,15 @@ export function markupCardBookInfo(data) {
     <svg class="close-modal-info" width="24" height="24">
         <use href="./images/symbol-defs.svg#close"></use>
     </svg>
-</button>
-<img src="${book_image}" alt="${title}" class="book_info_img">
-        <h2 class="book_info_name">${list_name}</h2>
-        <p class="book_info_author">${author}</p>
-        <p class="book_info_description">${description}</p>
-        <div class="info-logo-container">
+    </button>
+    <div class="info_img-author_container">
+        <img src="${book_image}" alt="${title}" class="book_info_img">
+
+        <div class="info_about_book">
+            <h2 class="book_info_name">${list_name}</h2>
+            <p class="book_info_author">${author}</p>
+            <p class="book_info_description">${description}</p>
+            <div class="info-logo-container">
             <a href="${buy_links[0].url}" class="book-store-link" target="_blank">
                 <img srcset="/src/images/trading-platforms/amazon.png 1x,
                 /src/images/trading-platforms/amazon@2x.png 2x"
@@ -30,6 +33,8 @@ export function markupCardBookInfo(data) {
               src="/src/images/trading-platforms/bookshop.png"  
                 alt="Bookshop" class="store-info-book" width="38" height="36">
             </a>
+        </div>
+        </div>
         </div>
         <button class="btn-book-info book_add__to_list" type="button" id="addRemoveBookButton">ADD TO SHOPPING LIST</button>
         <p class="book-infoBtn-explanation is-hidden">
