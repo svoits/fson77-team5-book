8606ca6489378262f2a251345b98fcef2edc1ca8
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-05-13 09:37:50 +0200

    fix formatting and some names

--- src/js/support-list.js
+++ src/js/support-list.js
@@ -1,75 +1,75 @@
-import actionAgainstHunger from '../images/support-ukraine/against-hunger.png'
-import actionAgainstHunger2 from '../images/support-ukraine/against-hunger@2x.png'
-import internationalMedicalCorps from '../images/support-ukraine/international-corps.png'
-import internationalMedicalCorps2 from '../images/support-ukraine/international-corps@2x.png'
-import medicinsSansFrontieres from '../images/support-ukraine/medecins-sans.png'
-import medicinsSansFrontieres2 from '../images/support-ukraine/medecins-sans@2x.png'
-import projectHope from '../images/support-ukraine/project-hope.png'
-import projectHope2 from '../images/support-ukraine/project-hope@2x.png'
-import razom from '../images/support-ukraine/razom.png'
-import razom2 from '../images/support-ukraine/razom@2x.png'
-import saveTheChildren from '../images/support-ukraine/save-the-children.png'
-import saveTheChildren2 from '../images/support-ukraine/save-the-children@2x.png'
-import serhiyPrytulaFoundation from '../images/support-ukraine/prytula.png'
-import serhiyPrytulaFoundation2 from '../images/support-ukraine/prytula@2x.png'
-import united24 from '../images/support-ukraine/united24.png'
-import united242 from '../images/support-ukraine/united24@2x.png'
-import worldVision from '../images/support-ukraine/world-vision.png'
-import worldVision2 from '../images/support-ukraine/world-vision@2x.png'
+import actionAgainstHunger from '../images/support-ukraine/against-hunger.png';
+import actionAgainstHunger_2x from '../images/support-ukraine/against-hunger@2x.png';
+import internationalMedicalCorps from '../images/support-ukraine/international-corps.png';
+import internationalMedicalCorps_2x from '../images/support-ukraine/international-corps@2x.png';
+import medicinsSansFrontieres from '../images/support-ukraine/medecins-sans.png';
+import medicinsSansFrontieres_2x from '../images/support-ukraine/medecins-sans@2x.png';
+import projectHope from '../images/support-ukraine/project-hope.png';
+import projectHope_2x from '../images/support-ukraine/project-hope@2x.png';
+import razom from '../images/support-ukraine/razom.png';
+import razom_2x from '../images/support-ukraine/razom@2x.png';
+import saveTheChildren from '../images/support-ukraine/save-the-children.png';
+import saveTheChildren_2x from '../images/support-ukraine/save-the-children@2x.png';
+import serhiyPrytulaFoundation from '../images/support-ukraine/prytula.png';
+import serhiyPrytulaFoundation_2x from '../images/support-ukraine/prytula@2x.png';
+import united24 from '../images/support-ukraine/united24.png';
+import united24_2x from '../images/support-ukraine/united24@2x.png';
+import worldVision from '../images/support-ukraine/world-vision.png';
+import worldVision_2x from '../images/support-ukraine/world-vision@2x.png';
 
 export const supportArr = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
     img: saveTheChildren,
-    img2x: saveTheChildren2,
+    img2x: saveTheChildren_2x,
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
     img: projectHope,
-    img2x: projectHope2,
+    img2x: projectHope_2x,
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
     img: united24,
-    img2x: united242,
+    img2x: united24_2x,
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
     img: internationalMedicalCorps,
-    img2x: internationalMedicalCorps2,
+    img2x: internationalMedicalCorps_2x,
   },
   {
     title: 'Medecins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
     img: medicinsSansFrontieres,
-    img2x: medicinsSansFrontieres2,
+    img2x: medicinsSansFrontieres_2x,
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
     img: razom,
-    img2x: razom2,
+    img2x: razom_2x,
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
     img: actionAgainstHunger,
-    img2x: actionAgainstHunger2,
+    img2x: actionAgainstHunger_2x,
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
     img: worldVision,
-    img2x: worldVision2,
+    img2x: worldVision_2x,
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
     img: serhiyPrytulaFoundation,
-    img2x: serhiyPrytulaFoundation2,
+    img2x: serhiyPrytulaFoundation_2x,
   },
 ];
