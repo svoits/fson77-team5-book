beee8140a0acea75d466fb1419d22b53595a21a7
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-05-13 10:19:28 +0200

    add header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-// import onCheckBoxClick from './js/theme-toggle';
 import './js/window-resize.js';
+import './js/header';
 import './js/render-categories';
 import './js/render-support';
 import './js/renderBestsellers';
+import './js/theme-toggle';
 import './js/modal-auth';
 import './js/firebase-auth';
