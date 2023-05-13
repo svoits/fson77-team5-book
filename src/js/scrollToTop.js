1e9139ad385927d097db8ef2eafd96d1ccf9c66c
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-05-13 10:00:44 +0200

    fix

--- src/js/scrollToTop.js
+++ src/js/scrollToTop.js
@@ -1,6 +1,6 @@
-export function scrollToTop(){
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
+export function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 }
