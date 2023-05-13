d6f5d0119ef7e9d8fff7417d3b6bcf847e33c225
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-05-13 20:43:49 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // import onCheckBoxClick from './js/theme-toggle';
+import './js/modal-team';
 import './js/renderBestsellers';
 import './js/render-categories';
 import './js/modal-auth';
 import './js/firebase-auth';
-import './js/modal-team';
-import './js/team-gallery';
