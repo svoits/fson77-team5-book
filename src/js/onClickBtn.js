e8338426ec9f361560ffb6f83e1bee8230a7e6a1
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-05-14 14:35:12 +0100

    update parseDiv and fix bugs

--- src/js/onClickBtn.js
+++ src/js/onClickBtn.js
@@ -9,13 +9,14 @@ export function onClickBtn(evt) {
   //
   const notification = document.querySelector('.book-infoBtn-explanation');
   if (evt.target.classList.contains('book_add__to_list')) {
-    addToLocalStorage(parseDiv(evt.target.closest('div')));
+    addToLocalStorage(parseDiv(evt.target.closest('div').children[1]));
+    console.dir(evt.target.closest('div').children[1]);
     evt.target.classList.add('book_remove__from_list');
     evt.target.classList.remove('book_add__to_list');
     evt.target.innerText = 'REMOVE FROM THE SHOPPING LIST';
     notification.classList.remove('visually-hidden');
   } else if (evt.target.classList.contains('book_remove__from_list')) {
-    removeFromLocaleStorage(parseDiv(evt.target.closest('div')));
+    removeFromLocaleStorage(parseDiv(evt.target.closest('div').children[1]));
     evt.target.classList.add('book_add__to_list');
     evt.target.classList.remove('book_remove__from_list');
     evt.target.innerText = 'ADD TO THE SHOPPING LIST';
--- src/js/parseDiv.js
+++ src/js/parseDiv.js
@@ -1,28 +1,34 @@
 export function parseDiv(div) {
   const dataToLocalStorage = {};
-  [...div.children].forEach(i => {
-    switch (i.tagName) {
-      case 'H2':
-        dataToLocalStorage.list_name = i.textContent;
-        break;
-      case 'IMG':
-        dataToLocalStorage.book_image = i.src;
-        dataToLocalStorage.title = i.alt;
-        break;
-      case 'P':
-        if (i.classList.contains('book_info_author')) {
-          dataToLocalStorage.author = i.textContent;
-        } else if (i.classList.contains('book_info_description')) {
-          const descr = i.textContent
-            ? i.textContent
-            : `Опис можете подивитись на сайті)`;
-          dataToLocalStorage.description = descr;
-        }
-        break;
-      case 'DIV':
-        const buyLinks = [...i.children].map(idx => idx.href);
-        dataToLocalStorage.buy_links = buyLinks;
-    }
+  [...div.children].forEach(childDivs => {
+    [...childDivs.children].forEach(childElementsDiv => {
+      switch (childElementsDiv.tagName) {
+        case 'H2':
+          console.log(childElementsDiv.textContent);
+          dataToLocalStorage.title = childElementsDiv.textContent;
+          break;
+        case 'IMG':
+          dataToLocalStorage.book_image = childElementsDiv.children.src;
+          // dataToLocalStorage.title = childElementsDiv.children.alt;
+          break;
+        case 'P':
+          if (childElementsDiv.classList.contains('book_info_author')) {
+            dataToLocalStorage.author = childElementsDiv.textContent;
+          } else if (
+            childElementsDiv.classList.contains('book_info_description')
+          ) {
+            const descr = childElementsDiv.textContent
+              ? childElementsDiv.textContent
+              : `Опис можете подивитись на сайті)`;
+            dataToLocalStorage.description = descr;
+          }
+          break;
+        case 'DIV':
+          const buyLinks = [...childElementsDiv.children].map(idx => idx.href);
+          dataToLocalStorage.buy_links = buyLinks;
+      }
+      return dataToLocalStorage;
+    });
   });
   return dataToLocalStorage;
 }
