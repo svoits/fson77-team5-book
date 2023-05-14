17a33ca9b265f8bbc7e67964d7701243371b0dea
Natalya Demidenko <natalucia777@ukr.net>
2023-05-14 04:40:48 +0300

    footer-10

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -29,48 +29,31 @@ function onTeamLogoClick(e) {
 function markup(data) {
   return data
     .map(({ original, username, position, git }) => {
-      return `<li class="team-member">
-      <img src="${original}" alt="${username}" />
-      <p>${username}</p>
-      <p>${position}</p>
-      <a href="${git}">GitHub</a>
-    </li>`;
+      return `<div class="team-box"> <li class="team-member">
+      <img class="team-member-img" src="${original}" alt="${username}" />
+      <p class="team-member-name">${username}</p>
+      <p class="team-member-title">${position}</p>
+      <a class="team-member-link" href="${git}">GitHub</a>
+    </li></div>`;
     })
     .join('');
 }
 
 function teamMarkUp(data) {
   return `<div class="team-wrapper">
-    <button class="team-modal-close" type="button">
+    <button class="team-modal-close" type="button" js-modal-close>
       <svg class="close-modal-info" width="24" height="24">
-        <use href="./images/symbol-defs.svg#close"></use>
+        <use class="close-modal-svg"  href="./images/symbol-defs.svg#close"></use>
       </svg>
     </button>
+    <h2 class="team-modal-title">NOVEL DEVS</h2>
     <ul class="team-list">${data}</ul>
   </div>`;
 }
 
-// galleryContent.innerHTML = markup(teamgallery);
-// //
-// galleryContent.addEventListener('click', onGetImage);
-// function onGetImage(e) {
-//   e.preventDefault();
-//   const { target } = e;
-//   if (!target.classList.contains('gallery__img')) {
-//     return;
-//   }
-//   //
-//   const instance = basicLightbox.create(
-//     `<img src="${target.dataset.source}"/>`,
-//     {
-//       onShow: () => window.addEventListener('keydown', onEscButton),
-//       onClose: () => window.removeEventListener('keydown', onEscButton),
-//     }
-//   );
-//   instance.show();
-//   function onEscButton(e) {
-//     if (e.code === 'Escape') {
-//       instance.close();
-//     }
-//   }
-// }
+const modalCloseBtn = document.querySelector('[js-modal-close]');
+modalCloseBtn.addEventListener('click', onModalCloseBtnClick);
+
+function onModalCloseBtnClick(e) {
+  modalInstance.close();
+}
