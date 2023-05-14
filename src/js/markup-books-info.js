0adb822e7cb71f17f001b3ab9d3ccee38a078e66
Serhii Voitsekhovskyi <95.ssh.ssh@gmail.com>
2023-05-14 14:09:59 +0200

    fix bug

--- src/js/markup-books-info.js
+++ src/js/markup-books-info.js
@@ -13,46 +13,68 @@ export function markupCardBookInfo(data, flag) {
     : 'ADD TO SHOPPING LIST';
   const buttonClass = flag ? 'book_remove__from_list' : 'book_add__to_list';
   const { book_image, author, description, title, buy_links } = data;
-  return `
-    <div class="book_info_card">
-    <button class="modal-info-close" type="button" data-modal-close>
+  return `<div class="book_info_card">
+  <button class="modal-info-close" type="button" data-modal-close>
     <svg class="info-modal-close-icon" width="24" height="24">
-    <use href="${logos}#close"></use>
+      <use href="${logos}#close"></use>
     </svg>
-    </button>
-    <div class="info_img-author_container">
-        <div class="book-info-img-wrap"><img src="${book_image}" alt="${title}" class="book_info_img"></div>
+  </button>
+  <div class="info_img-author_container">
+    <div class="book-info-img-wrap">
+      <img src="${book_image}" alt="${title}" class="book_info_img" />
+    </div>
 
-        <div class="info_about_book">
-            <h2 class="book_info_name">${title}</h2>
-            <p class="book_info_author">${author}</p>
-            <p class="book_info_description">${description}</p>
-            <div class="info-logo-container">
-            <a href="${buy_links[0].url}" class="book-store-link" target="_blank">
-                <img srcset="${amazon} 1x,
-                ${amazon_2x} 2x"
-              src="${amazon}"
-                alt="Amazon" class="amazon-logo store-info-book" width="62" height="19">
-            </a>
-            <a href="${buy_links[1].url}" class="book-store-link" target="_blank">
-                <img srcset="${apple} 1x,
-                ${apple_2x} 2x"
-              src="${apple}" 
-                alt="Apple Books" class="store-info-book" width="33" height="32">
-            </a>
-            <a href="${buy_links[4].url}" class="book-store-link" target="_blank">
-                <img srcset="${bookshop} 1x,
-                ${bookshop_2x} 2x"
-              src="${bookshop}"  
-                alt="Bookshop" class="store-info-book" width="38" height="36">
-            </a>
-        </div>
-        <button class="btn-book-info ${buttonClass}" type="button" id="addRemoveBookButton">${buttonText}</button>
-        <p class="book-infoBtn-explanation ${classDescription}">
-        Сongratulations! You have added the book to the shopping list. 
-        To delete, press the button “Remove from the shopping list”.
-        </p>
-    </div>`;
+    <div class="info_about_book">
+      <h2 class="book_info_name">${title}</h2>
+      <p class="book_info_author">${author}</p>
+      <p class="book_info_description">${description}</p>
+      <div class="info-logo-container">
+        <a href="${buy_links[0].url}" class="book-store-link" target="_blank">
+          <img
+            srcset="${amazon} 1x, ${amazon_2x} 2x"
+            src="${amazon}"
+            alt="Amazon"
+            class="amazon-logo store-info-book"
+            width="62"
+            height="19"
+          />
+        </a>
+        <a href="${buy_links[1].url}" class="book-store-link" target="_blank">
+          <img
+            srcset="${apple} 1x, ${apple_2x} 2x"
+            src="${apple}"
+            alt="Apple Books"
+            class="store-info-book"
+            width="33"
+            height="32"
+          />
+        </a>
+        <a href="${buy_links[4].url}" class="book-store-link" target="_blank">
+          <img
+            srcset="${bookshop} 1x, ${bookshop_2x} 2x"
+            src="${bookshop}"
+            alt="Bookshop"
+            class="store-info-book"
+            width="38"
+            height="36"
+          />
+        </a>
+      </div>
+    </div>
+    </div>
+    <button
+      class="btn-book-info ${buttonClass}"
+      type="button"
+      id="addRemoveBookButton"
+    >
+      ${buttonText}
+    </button>
+    <p class="book-infoBtn-explanation ${classDescription}">
+      Сongratulations! You have added the book to the shopping list. To delete,
+      press the button “Remove from the shopping list”.
+    </p>
+</div>
+`;
 }
 
 // REMOVE FROM SHOPPING LIST
